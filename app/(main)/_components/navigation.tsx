1f6357f3764e3220b45211c73998df0117d2d543
jhlee <wisdom_13_@naver.com>
2024-03-09 19:54:13 +0900

    🔧 Sidebar Item

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,21 +1,23 @@
 'use client';
 
-import { ChevronsLeft, MenuIcon } from 'lucide-react';
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
-import { useQuery } from 'convex/react';
+import { useMutation, useQuery } from 'convex/react';
+import { toast } from 'sonner';
 
 import { cn } from '@/lib/utils';
 import { api } from '@/convex/_generated/api';
 
 import { UserItem } from './user-item';
+import { Item } from './item';
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
-
   const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizeingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<'aside'>>(null);
@@ -99,6 +101,16 @@ export const Navigation = () => {
     }
   }
 
+  const handleCreate = () => {
+    const promise = create({title: 'Untitled'});
+
+    toast.promise(promise, {
+      loading: 'Creating a new note...',
+      success: 'New note create!',
+      error: 'Failed to create a new note'
+    })
+  }
+
   return (
     <>
       <aside
@@ -119,6 +131,22 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item
+            label="Search"
+            icon={Search}
+            isSearch
+            onClick={() => {}}
+          />
+          <Item
+            label="Settings"
+            icon={Settings}
+            onClick={() => {}}
+          />
+          <Item
+            label="New page"
+            icon={PlusCircle}
+            onClick={handleCreate}
+          />
         </div>
         <div className='mt-4'>
           {documents?.map((document) => (
