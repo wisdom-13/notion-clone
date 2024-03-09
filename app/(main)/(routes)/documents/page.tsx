0a729586182c5835826e14f40defd1e1451c1bc0
jhlee <wisdom_13_@naver.com>
2024-03-09 18:43:08 +0900

    🔧 Schema & API

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,11 +3,26 @@
 import Image from 'next/image';
 import { PlusCircle } from 'lucide-react';
 import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
+import { toast } from 'sonner';
 
+import { api } from '@/convex/_generated/api';
 import { Button } from '@/components/ui/button';
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({title: 'Untitled'});
+
+    toast.promise(promise, {
+      loading: 'Creating a new note...',
+      success: 'New note create!',
+      error: 'Failed to create a new note'
+    })
+  }
+
   return (
     <div className='h-full flex flex-col items-center justify-center space-y-4'>
       <Image
@@ -27,7 +42,7 @@ const DocumentsPage = () => {
       <h2 className='text-lg font-medium'>
         Welcom to {user?.firstName}&apos;s Zite
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className='h-4 w-t mr-2'/>
         Create a note
       </Button>
