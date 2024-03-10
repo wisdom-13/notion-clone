a4896330545b56901b05a7b84145dbd7ec51edfd
jhlee <wisdom_13_@naver.com>
2024-03-11 01:28:26 +0900

    🔧 Navbar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from 'lucide-react';
-import { usePathname } from 'next/navigation';
+import { useParams, usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 import { useMutation } from 'convex/react';
@@ -21,10 +21,12 @@ import { UserItem } from './user-item';
 import { Item } from './item';
 import { DocumentList } from './document-list';
 import { TrashBox } from './trash-box';
+import { Navbar } from './navbar';
 
 export const Navigation = () => {
   const search = useSearch();
   const settings = useSettings();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
   const create = useMutation(api.documents.create);
@@ -190,9 +192,17 @@ export const Navigation = () => {
         isResetting && 'transition-all ease-in-out duration-300',
         isMobile && 'left-0 w-full')}
       >
-        <nav className='bg-transparent px-3 py-2 w-full'>
-          {isCollapsed && <MenuIcon onClick={resetWidth} role='button' className='h-6 w-6 text-muted-foreground'/>}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar
+            isCollapsed={isCollapsed}
+            onResetWidth={resetWidth}
+          />
+        ) : (
+          <nav className='bg-transparent px-3 py-2 w-full'>
+            {isCollapsed && <MenuIcon onClick={resetWidth} role='button' className='h-6 w-6 text-muted-foreground'/>}
+          </nav>
+        )}
+        
       </div>
     </>
   )
