0b8a997b14100e79154d5b1a1baa37e6c75d26f9
jhlee <wisdom_13_@naver.com>
2024-03-12 02:34:50 +0900

    🧹 Editor performance optimization

--- components/editor.tsx
+++ components/editor.tsx
@@ -19,7 +19,7 @@ interface EditorProps {
   editable?: boolean;
 };
 
-export const Editor = ({
+const Editor = ({
   onChange,
   initialContent,
   editable
@@ -53,3 +53,5 @@ export const Editor = ({
     </div>
   )
 }
+
+export default Editor;
